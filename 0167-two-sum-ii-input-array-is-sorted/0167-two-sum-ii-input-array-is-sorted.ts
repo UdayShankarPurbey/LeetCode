f51cdce3879fca1f8f9b5c1754df0399f99c5510
Uday Shankar Purbey <144823786+UdayShankarPurbey@users.noreply.github.com>
2026-08-12 13:52:44 +0530

    Time: 2 ms (58.44%), Space: 57.4 MB (7.91%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.ts
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.ts
@@ -1,14 +1,11 @@
 function twoSum(numbers: number[], target: number): number[] {
-    const remainerTarget = new Map();
-
-    for(let i = 0 ; i<numbers.length; i++) {
+    const remainderMap = new Map();
+    for(let i = 0; i<numbers.length; i++) {
         const num = numbers[i];
-        const reaminder = target - num 
-        if(remainerTarget.has(num)) {
-            return [(remainerTarget.get(num) + 1),(i + 1)]
-        }
-        else {
-            remainerTarget.set(reaminder , i)
-        }
+        if(remainderMap.has(num)) return[remainderMap.get(num),i+1];
+
+        remainderMap.set((target - num) , i+1);
     }
-};
+
+    return [];
+}
