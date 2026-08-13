fee19a6b7d343b08f13ce9f7580fa8214a20b33c
Uday Shankar Purbey <144823786+UdayShankarPurbey@users.noreply.github.com>
2026-08-13 16:07:03 +0530

    Time: 2 ms (47.5%), Space: 57.6 MB (23.75%) - LeetHub

--- 1464-maximum-product-of-two-elements-in-an-array/1464-maximum-product-of-two-elements-in-an-array.ts
+++ 1464-maximum-product-of-two-elements-in-an-array/1464-maximum-product-of-two-elements-in-an-array.ts
@@ -1,22 +1,5 @@
-function maxProduct(nums: number[]): number {
-    const numLength = nums.length;
-    const possiblePair = [];
-    let max = 0;
-
-    for(let i = 0 ; i<numLength;i++) {
-        const num = nums[i]
-        for(let j= i+1; j<numLength;j++) {
-            possiblePair.push([num , nums[j]])
-        }
-    }
-
-    for(let pp of possiblePair) {
-        const left = pp[0] 
-        const right = pp[1]
-        const productValue = (left-1)*(right-1)
-        if(max<productValue) {
-            max = productValue
-        }
-    }
-    return max;
-};
+function maxProduct(nums: number[]): number { 
+    if(nums.length<2) return 0;
+    nums.sort((a : number , b: number) => b - a);
+    return (nums[0] - 1) * (nums[1] -1);
+}
